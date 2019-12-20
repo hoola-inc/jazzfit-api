264d3299b121f047a42bf4b94e0c71ccaf1e45b0
Ameer <muhammadameerhmza@gmail.com>
2019-12-20 17:04:16 +0500

    route change

--- src/app/routes/survey.route.js
+++ src/app/routes/survey.route.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const controller = require('../contollers/survey-one.controller');
     const validateToken = require('../../utilities/jwt.utils').validateToken;
     app.post('/surveyone', controller.questions);
-    app.get('/questions/:name', validateToken, controller.getAllQuestions);
+    app.get('/questions', validateToken, controller.getAllQuestions);
 };
 
 
