8502549e22d62fd96f54fb16394a316e8c332400
Ameer <muhammadameerhmza@gmail.com>
2020-01-03 19:26:27 +0500

    score changes

--- src/utilities/recommendations.utils.js
+++ src/utilities/recommendations.utils.js
@@ -57,7 +57,7 @@ function physicalRecommendations(physicalElement) {
 function mentalRecommendations(mentalElement) {
 
     if (mentalElement.questionText === mentalRecommendationsConst.Questionone.questionTitle) {
-        if (mentalElement.answerText === 'Never' || mentalElement.answerText === 'Sometimes')
+        if (mentalElement.answerText === 'Always' || mentalElement.answerText === 'Often')
             createObj(mentalRecommendationsConst.Questionone.recommendationTitle, mentalRecommendationsConst.Questionone.recommendationArr, mentalElement, mentalRecommendationsConst.color);
         else
             correctAnswerObj(mentalElement, mentalRecommendationsConst.Questionone.recommendationForCorrectAnswerArr, mentalRecommendationsConst.color);
