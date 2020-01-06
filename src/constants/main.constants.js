6d3fc93043d40e873698daa082d24d3b3401c524
Ameer <muhammadameerhmza@gmail.com>
2020-01-06 13:25:31 +0500

    text changes

--- src/constants/main.constants.js
+++ src/constants/main.constants.js
@@ -8,31 +8,31 @@ module.exports.totalWellness = {
 }
 
 module.exports.physicalWellness = {
-    Fourth: "You have a significant need to manage your physical wellbeing. A major lifestyle change is needed at this point. Continued negligence may lead to health problems and drop in productivity at work. To view details of your results, tap/click on the bar chart. Note: Detailed recommendations will be given (covered in the next segment)",
-    Third: "You are not managing your physical wellbeing; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your physical wellbeing, click/tap on the bar chart. Note: Detailed recommendations will be given (covered in the next segment)",
-    Second: "You are good at managing your physical wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your physical wellbeing, click/tap on the bar chart. Note: Detailed recommendations will be given (covered in the next segment)",
-    First: "You are excellent at managing your physical wellbeing. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently. Note: Detailed recommendations will be given (covered in the next segment)"
+    Fourth: "You have a significant need to manage your physical wellbeing. A major lifestyle change is needed at this point. Continued negligence may lead to health problems and drop in productivity at work. To view details of your results, tap/click on the click/tab below.",
+    Third: "You are not managing your physical wellbeing; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your physical wellbeing, click/tap on the click/tab below.",
+    Second: "You are good at managing your physical wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your physical wellbeing, click/tap on the click/tab below.",
+    First: "You are excellent at managing your physical wellbeing. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently."
 }
 
 module.exports.emotionalWellness = {
     Fourth: "You have a significant need to manage your emotional wellbeing. A major lifestyle change is needed at this point. Continued negligence may lead to anxiety, self-image concerns, physical health problems and relationship issues, with people at home and at work.",
-    Third: "You are not managing your emotional well-being; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your emotional wellbeing, click/tap on the bar chart.",
-    Second: "You are good at managing your emotional wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your emotional wellbeing, click/tap on the bar chart.",
-    First: "You are excellent at managing your emotional well-being. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently. Note: Detailed recommendations will be given (covered in the next segment)"
+    Third: "You are not managing your emotional well-being; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your emotional wellbeing, click/tap on the click/tab below.",
+    Second: "You are good at managing your emotional wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your emotional wellbeing, click/tap on the click/tab below.",
+    First: "You are excellent at managing your emotional well-being. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently."
 }
 
 module.exports.mentalWellness = {
     Fourth: "You have a significant need to manage your mental wellbeing. A major lifestyle change is needed at this point. Continued negligence may lead to anxiety, distractions, lack of focus and burn-out.",
-    Third: "You are not managing your mental well-being; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your mental well-being, click/tap on the bar chart.",
-    Second: "You are good at managing your mental wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your mental wellbeing, click/tap on the bar chart.",
-    First: " You are excellent at managing your mental well-being. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently. Note: Detailed recommendations will be given (covered in the next segment)"
+    Third: "You are not managing your mental well-being; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your mental well-being, click/tap on the click/tab below.",
+    Second: "You are good at managing your mental wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your mental wellbeing, click/tap on the click/tab below.",
+    First: " You are excellent at managing your mental well-being. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently."
 }
 
 module.exports.socialWellness = {
     Fourth: "You have a significant need to manage your social wellbeing. A major lifestyle change is needed at this point. Continued negligence may lead to anxiety, self-image concerns, physical health problems and relationship issues, with people at home and at work.",
-    Third: "You are not managing your social well-being; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your social wellbeing, click/tap on the bar chart.",
-    Second: "You are good at managing your social wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your social wellbeing, click/tap on the bar chart.",
-    First: "You are excellent at managing your social well-being. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently. Note: Detailed recommendations will be given (covered in the next segment)"
+    Third: "You are not managing your social well-being; certain aspects of your life need to be addressed soon or your energy will further drop which will likely lead to adverse consequences for your health and social life. To view methods to improve your social wellbeing, click/tap on the click/tab below.",
+    Second: "You are good at managing your social wellbeing; however, there are areas which need your immediate attention. Little changes can lead to a much better and productive lifestyle. To view methods to improve your social wellbeing, click/tap on the click/tab below.",
+    First: "You are excellent at managing your social well-being. Nonetheless, you may look up methods to specifically address the areas which may need some improvement. Other than that, your focus should be on maintaining the practices you partake in currently."
 }
 
 // Recommendations against each score
