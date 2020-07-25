27ed54534099a717a4cf431e0f95bafe4ff6e3bc
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2020-07-25 23:07:22 +0500

    admin dashboard user detail api

--- src/app/contollers/total-score-weightage.controller.js
+++ src/app/contollers/total-score-weightage.controller.js
@@ -1,4 +1,6 @@
 const totalScoreWeightageSchema = require('../models/total-score-weightage.model');
+const qaSchema = require('../models/question-answer.model');
+const recommendationSchema = require('../models/recommendation.model');
 const response = require('../../utilities/reponse.utils');
 exports.total = async (req, res, next) => {
     try {
@@ -12,6 +14,24 @@ exports.total = async (req, res, next) => {
             throw Error('no record found! ');
 
     } catch (error) {
+        next(error);
+    }
+}
+
+exports.totalEmpRecords = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const totalScore = await totalScoreWeightageSchema.find({ empId: id }).lean();
+        const qa = await qaSchema.find({ empId: id }).lean();
+        const recommendations = await recommendationSchema.find({ empId: id }).lean();
+        const responseObj = {
+            totalScore: totalScore,
+            questionAnswers: qa,
+            recommendations: recommendations
+        }
+        response.SUCCESS(res, responseObj);
+    } catch (error) {
+        console.log(error);
         next(error);
     }
 }
--- src/app/routes/total-score-weightage.route.js
+++ src/app/routes/total-score-weightage.route.js
@@ -2,4 +2,6 @@ module.exports = app => {
     const controller = require('../contollers/total-score-weightage.controller');
     const validateToken = require('../../utilities/jwt.utils').validateToken;
     app.get('/totalscore/:id', validateToken, controller.total);
+
+    app.get('/totalemprecords/:id', controller.totalEmpRecords);
 };
