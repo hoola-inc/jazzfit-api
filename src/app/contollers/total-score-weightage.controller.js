d7a542e8f640769c981c5bc97ce0154d1a6b6b28
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2020-07-26 05:45:48 +0500

    WIP

--- src/app/contollers/total-score-weightage.controller.js
+++ src/app/contollers/total-score-weightage.controller.js
@@ -1,6 +1,6 @@
 const totalScoreWeightageSchema = require('../models/total-score-weightage.model');
-const qaSchema = require('../models/question-answer.model');
 const recommendationSchema = require('../models/recommendation.model');
+const empSchema = require('../models/employee.model');
 const response = require('../../utilities/reponse.utils');
 exports.total = async (req, res, next) => {
     try {
@@ -22,11 +22,11 @@ exports.totalEmpRecords = async (req, res, next) => {
     try {
         const id = req.params.id;
         const totalScore = await totalScoreWeightageSchema.find({ empId: id }).lean();
-        const qa = await qaSchema.find({ empId: id }).lean();
         const recommendations = await recommendationSchema.find({ empId: id }).lean();
+        const empRecords = await empSchema.find({ empId: id }).lean();
         const responseObj = {
+            employee: empRecords,
             totalScore: totalScore,
-            questionAnswers: qa,
             recommendations: recommendations
         }
         response.SUCCESS(res, responseObj);
